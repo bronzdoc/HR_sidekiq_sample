9820902b6990fe129b01ab2bb3feba2e28cfeebd
bronzdoc <lsagastume1990@gmail.com>
2024-10-17 11:29:34 -0600

    Add import functionality to sales#import

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -2,4 +2,23 @@ class SalesController < ApplicationController
   def index
     render json: Sale.order(:id)
   end
+
+  def import
+    file = params[:file]
+
+    if file.blank?
+      render json: {}, status: :unprocessable_entity
+      return
+    end
+
+    content_type = file.content_type
+    unless content_type == "text/csv"
+      render json: {}, status: :unprocessable_entity
+      return
+    end
+
+    csv_content = file.read
+    job_id = SalesImportJob.perform_async(csv_content)
+    render json: { job_id: job_id }, status: :ok
+  end
 end
